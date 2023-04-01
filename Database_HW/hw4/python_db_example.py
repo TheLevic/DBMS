df8ae26ae74b3faa09d651b98401d7c7265e7d19
TheLevic <levicrider08@gmail.com>
2023-04-01 11:47:16 -0500

    Added run script, and Menu

--- Database_HW/hw4/python_db_example.py
+++ Database_HW/hw4/python_db_example.py
@@ -52,35 +52,56 @@ def close_db():  # use this function to close db
 
 
 ##### Test #######
+
+# print(' ')
+# print('Testing select: ')
+# print('=======================================')
+# executeSelect('SELECT * FROM DEPT')
+
+# print(' ')
+# print('\nTesting insert of dept MATH:')
+# print('=======================================')
+# insert("DEPT", "'MATH', 'Mathematics', 309, 'SCEN'")
+# executeSelect('SELECT * FROM DEPT WHERE DEPT_CODE = "MATH";')
+
+# print(' ')
+# print('\nTesting delete of dept MATH:')
+# print('=======================================')
+# executeUpdate('DELETE FROM DEPT WHERE DEPT_CODE = "MATH";')
+# executeSelect('SELECT * FROM DEPT WHERE DEPT_CODE = "MATH";')
+
+# print(' ')
+# print('\nTesting update of professor name :')
+# print('=======================================')
+# executeSelect("SELECT * FROM PROFESSOR WHERE PROF_ID = 123456;")
+# executeUpdate("Update PROFESSOR set PROF_NAME = 'Susan Dyer' WHERE PROF_ID = 123456;")
+# executeSelect("SELECT * FROM PROFESSOR WHERE PROF_ID = 123456;")
+
+
 mysql_username = 'lccrider'  # please change to your username
 mysql_password = 'rao8eeMu'  # please change to your MySQL password
+try:
+    open_database('localhost', mysql_username, mysql_password, mysql_username)  # open database
+    print("Database opened successfully\n\n")
+except Exception as e:
+    print("Error: " + e)
+    exit(1)
 
-open_database('localhost', mysql_username, mysql_password,
-              mysql_username)  # open database
+choice = 0
 
-print(' ')
-print('Testing select: ')
-print('=======================================')
-executeSelect('SELECT * FROM DEPT')
-
-print(' ')
-print('\nTesting insert of dept MATH:')
-print('=======================================')
-insert("DEPT", "'MATH', 'Mathematics', 309, 'SCEN'")
-executeSelect('SELECT * FROM DEPT WHERE DEPT_CODE = "MATH";')
-
-print(' ')
-print('\nTesting delete of dept MATH:')
-print('=======================================')
-executeUpdate('DELETE FROM DEPT WHERE DEPT_CODE = "MATH";')
-executeSelect('SELECT * FROM DEPT WHERE DEPT_CODE = "MATH";')
-
-print(' ')
-print('\nTesting update of professor name :')
-print('=======================================')
-executeSelect("SELECT * FROM PROFESSOR WHERE PROF_ID = 123456;")
-executeUpdate("Update PROFESSOR set PROF_NAME = 'Susan Dyer' WHERE PROF_ID = 123456;")
-executeSelect("SELECT * FROM PROFESSOR WHERE PROF_ID = 123456;")
+while choice != "6":
+    print("Please select on of the following options:")
+    print("1. Find Professors")
+    print("2. Find Sections")
+    print("3. Add Section")
+    print("4. Update Section")
+    print("5. Report Enrollment")
+    print("6. Quit")
+    choice = input("Enter your choice: ")
+    
+    if choice == "1":
+        # Select all sections in the spection table
+        executeSelect("SELECT * FROM DEPT;")
 
 close_db()  # close database
 
---
+++ Database_HW/hw4/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sftp lccrider@turing.uark.edu <<EOF
+put python_db_example.py
+put runpython.sh
+exit
+EOF
+ssh lccrider@turing.uark.edu <<EOF
+chmod +x runpython.sh
+./runpython.sh
+1
+6
+EOF
